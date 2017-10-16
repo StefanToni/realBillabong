00c5757096402ecbc00f7ed064840b3af5765260
Kamil Inglot <outglot@gmail.com>
2017-10-16 18:47:46 +0200

    Stuff

--- realBillabong/src/Game/HumanPlayer.java
+++ realBillabong/src/Game/HumanPlayer.java
@@ -2,10 +2,14 @@ package Game;
 
 import java.util.ArrayList;
 
+import realBillabong.Main;
+
 public class HumanPlayer extends Player {  
 	
 	
 	private int team ;
+	private MouseAdapter mouse ;
+	private int x, y, actualX, actualY ;
 	
 	
 
@@ -13,10 +17,32 @@ public class HumanPlayer extends Player {
 		// TODO Auto-generated constructor stub
 		super.setTeamCounter(super.getTeamCounter() + 1) ;
 		team = super.getTeamCounter() ;
+		mouse = Main.getState().getMouse() ;
+	
+	}
+	
+	public void placePiece(){
+		x = mouse.getX();
+		y = mouse.getY();
+		 for(int i=0; i < 14; i++)
+         {
+             for(int j=0;j < 16 ;j++ )
+             {
+
+                 if((51+(20*j)<=x) && (69+(20*j)>=x) &&  (51+(20*i)<=y) && (69+(20*i)>=y) )  
+                 {
+                	 actualY = i;
+                	 actualX = j;
+                 }
+                 else{
+                	 System.out.println("smt went wrong...");
+                 }
 	}
 
 	public void makeMove(){
 		
 	}
+
+
 
 }
--- realBillabong/src/realBillabong/GameState.java
+++ realBillabong/src/realBillabong/GameState.java
@@ -1,12 +1,14 @@
 package realBillabong;
 
 import Game.Board;
+import Game.MouseAdapter;
 
 public interface GameState {
 	
 	public void render() ;
 	public void changeState(GameState s) ;
 	public void update() ;
+	public MouseAdapter getMouse() ;
 	//public Board getBoard();
 	
 
