e0c6cee88fbbfc35b3e1d531a842ff6c603a4a12
StefanToni <skatemusic2301@gmail.com>
2017-10-18 15:32:04 +0200

    selectahs added to git (hac to add manual)

---
+++ realBillabong/src/Game/SquareSelectah.java
@@ -0,0 +1,96 @@
+package Game;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import realBillabong.Main;
+
+public class SquareSelectah implements MouseListener {
+	
+	private boolean done = false ;
+	private int x, y, actualX, actualY ;
+	private Square selected ;
+
+	public int getActualX() {
+		return actualX;
+	}
+
+	public void setActualX(int actualX) {
+		this.actualX = actualX;
+	}
+
+	public int getActualY() {
+		return actualY;
+	}
+
+	public void setActualY(int actualY) {
+		this.actualY = actualY;
+	}
+
+	public Square getSelected() {
+		return selected;
+	}
+
+	public void setSelected(Square selected) {
+		this.selected = selected;
+	}
+
+	public boolean isDone() {
+		return done;
+	}
+
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		x = e.getX();
+		y = e.getY();
+		System.out.println(x +" , " + y);
+		for(int i=0; i < 14; i++)
+        {
+            for(int j=0;j < 16 ;j++ )
+            {
+                if((79+(20*j)<=x) && (97+(20*j)>=x) &&  (56+(20*i)<=y) && (74+(20*i)>=y) )  
+                {
+               	 setActualY(i);
+               	 setActualX(j);
+            	 System.out.println(j+ " " + i + Main.getState().getLoop().getCurrentPlayer().getName());
+                }
+               
+            }
+        }
+		if (!Main.getState().getLoop().getBoard().getBoardArray()[actualY][actualX].isOccupied() ){
+			selected = Main.getState().getLoop().getBoard().getBoardArray()[actualY][actualX] ;
+			done = true ;
+		}
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
