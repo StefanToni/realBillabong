04b89a5a9f480373d96e61de143baec56a381bb9
MARTKNOBEN\Mart Knoben <mart0knoben@gmail.com>
2017-11-24 12:01:15 +0100

    Calculating new moves, minimax
    not finished, evaluation function can be used

--- realBillabong/src/AI/Evaluator.java
+++ realBillabong/src/AI/Evaluator.java
@@ -1,6 +1,27 @@
 package AI;
 
+import Game.*;
+
+
 public class Evaluator
 {
-
+	public int Evaluate(Board b, Kangaroo k, Square op, Square np) {
+		int score = 0;
+		int old_x = op.getxLoc();
+		int old_y = op.getyLoc();
+		int new_x = np.getxLoc();
+		int new_y = np.getyLoc();
+		
+		// Jump
+		if(k.checkLegal(old_x, old_y, new_x, new_y,np) && !(k.onlyOne(old_x, old_y, new_x, new_y))){
+			score += 1;
+		}
+		
+		// Walk
+		if((k.onlyOne(old_x, old_y, new_x, new_y) && !(k.checkLegal(old_x, old_y, new_x, new_y,np))){
+			score -= 1;
+		}
+				
+		return score;
+	}
 }
---
+++ realBillabong/src/AI/MoveCalculator.java
@@ -0,0 +1,65 @@
+package AI;
+
+import java.util.ArrayList;
+
+import Game.*;
+import realBillabong.*;
+
+public class MoveCalculator
+{
+	private Square[][] original_board;
+	private Square[][] new_board;
+	private ArrayList<Square[][]> next_boards;
+	private Kangaroo k;
+	private Kangaroo r;
+	private Kangaroo[] kangaroos;
+	
+	public ArrayList<Square[][]> getNextBoards() {
+		return next_boards;
+	}
+	
+	
+	public MoveCalculator(Kangaroo kanga) 
+	{
+		new_board = Main.getState().getLoop().getBoard().getBoardArray();
+		k = kanga;
+		r = Main.getState().getLoop().getBoard().getReferee();
+		kangaroos = Main.getState().getLoop().getBoard().getKangaroos();
+		
+		next_boards = getNextMoves(k);
+			
+	}
+	
+	private ArrayList<Square[][]> getNextMoves(Kangaroo k) 
+	{
+		
+		ArrayList<Square[][]> possible_moves = new ArrayList<Square[][]>();
+		
+		Square op = k.getPosition();
+		int old_x = op.getxLoc();
+		int old_y = op.getyLoc();
+		
+		Square np = new Square(0,0);
+		int new_x;
+		int new_y;
+		
+		for(int i = 0 ; i < 14 ; i++){
+			for(int j = 0 ; j < 16 ; j++) {
+				if(!(new_board[i][j].isOccupied() || new_board[i][j].isWater())) {
+					new_board[i][j].fill(k); // to get new coordinates
+					np = k.getPosition();
+					new_x = np.getxLoc();
+					new_y = np.getyLoc();
+					new_board[i][j].empty(); // to restore the board to the original gamestate before checking the move
+					
+					if(k.checkLegal(old_x, old_y, new_x, new_y,np)) {
+						new_board[i][j].fill(k); // to add the new move to the array of newboards
+						possible_moves.add(new_board);
+						new_board[i][j].empty(); // to restore the board to the original gamestate.
+					}				
+				}
+			}
+		}
+		return possible_moves;
+	}
+}
