1e393f80d39f28de5e19e17ff7eb41476a9bda51
Kamil Inglot <outglot@gmail.com>
2017-12-13 16:08:17 +0100

    Keyboard class created, now we can press space to run AI

---
+++ realBillabong/src/realBillabong/Keyboard.java
@@ -0,0 +1,28 @@
+package realBillabong;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Keyboard implements KeyListener {
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode()== KeyEvent.VK_SPACE) {
+			Main.getState().getLoop().aiMove();
+		}
+		
+	}
+
+}
